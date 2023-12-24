c97003d22d80d39459dd2367a9ba4dea790be8f0
Kristin Dailey <kristin.dailey02@gmail.com>
2023-12-24 12:00:00 -0800

    Updated solution

--- 7kyu/strings/vowel-count.js
+++ 7kyu/strings/vowel-count.js
@@ -37,8 +37,8 @@ function getCount(str) {
 function getCount(str) {
     let vowelsCount = 0;
     const vowels = ["a", "e", "i", "o", "u"];
-    for(let char of str) {
-        if(vowels.includes(char)) {
+    for (let char of str) {
+        if (vowels.includes(char)) {
             vowelsCount++;
         }
     }
