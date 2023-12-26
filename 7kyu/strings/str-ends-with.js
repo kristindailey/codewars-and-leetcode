00d672bebe1c5b6ebce72de12ab18e422a5e33bc
Kristin Dailey <kristin.dailey02@gmail.com>
2023-12-26 12:00:00 -0800

    Updated description

--- 7kyu/strings/str-ends-with.js
+++ 7kyu/strings/str-ends-with.js
@@ -1,10 +1,10 @@
 /* CHALLENGE DESCRIPTION
 
-Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+Complete the solution so that it returns true if the first argument (string) passed in ends with the second argument (string).
 
 Examples:
-solution('abc', 'bc') // returns true
-solution('abc', 'd') // returns false
+solution("abc", "bc") // returns true
+solution("abc", "d") // returns false
 */
 
 // function that takes in two params, both str
