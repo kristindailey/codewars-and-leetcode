5f1accfa9d0bb55dd255e0b219ee7977b2b46700
Kristin Dailey <kristin.dailey02@gmail.com>
2023-12-28 12:00:00 -0800

    Updated description

--- 7kyu/strings/reverse-words.js
+++ 7kyu/strings/reverse-words.js
@@ -1,6 +1,6 @@
 /*  CHALLENGE DESCRIPTION
 
-Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Complete the function that accepts a string parameter and reverses each word in the string. All spaces in the string should be retained.
 
 Examples:
 "This is an example!" ==> "sihT si na !elpmaxe"
