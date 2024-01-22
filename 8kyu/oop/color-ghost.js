cf4e8953c8798ad9fe26a7103091b7b4317b01ae
Kristin Dailey <kristin.dailey02@gmail.com>
2024-01-22 12:00:00 -0800

    Updated description

--- 8kyu/oop/color-ghost.js
+++ 8kyu/oop/color-ghost.js
@@ -4,10 +4,10 @@ Create a class Ghost.
 
 Ghost objects are instantiated without any arguments.
 
-Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated.
 
 ghost = new Ghost();
-ghost.color //=> "white" or "yellow" or "purple" or "red"
+ghost.color => "white" or "yellow" or "purple" or "red"
 */
 
 // MY SOLUTION
