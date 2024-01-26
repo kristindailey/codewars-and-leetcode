441e83c9d36b7328bd31d642ba32a4a933b3d5bb
Kristin Dailey <kristin.dailey02@gmail.com>
2024-01-26 12:00:00 -0800

    Updated description

--- 8kyu/strings/well-of-ideas.js
+++ 8kyu/strings/well-of-ideas.js
@@ -1,10 +1,10 @@
 /* CHALLENGE DESCRIPTION
 
-For every good kata idea there seem to be quite a few bad ones.
+Check the provided array (x) for good ideas "good" and bad ideas "bad." 
 
-In this kata you need to check the provided array (x) for good ideas "good" and bad ideas "bad". 
-If there are one or two good ideas, return "Publish!". If there are more than two, return "I smell a series!". 
-If there are no good ideas, as is often the case, return "Fail!".
+If there are one or two good ideas, return "Publish!" If there are more than two, return "I smell a series!"
+
+If there are no good ideas, as is often the case, return "Fail!"
 */
 
 // MY SOLUTION
