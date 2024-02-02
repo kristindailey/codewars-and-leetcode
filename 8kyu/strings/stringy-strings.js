95d6146f45bad51e0c08755095ee52f5bd6e9050
Kristin Dailey <kristin.dailey02@gmail.com>
2024-02-02 12:00:00 -0800

    Updated description

--- 8kyu/strings/stringy-strings.js
+++ 8kyu/strings/stringy-strings.js
@@ -1,14 +1,13 @@
 /* CHALLENGE DESCRIPTION
 
-Write a function stringy that takes a size and returns a string of alternating 1s and 0s.
+Write a function that takes a size and returns a string of alternating 1s and 0s.
 
 The string should start with a 1.
 
-A string with size 6 should return'101010'.
-
-With size 4 should return '1010'.
-
-With size 12 should return '101010101010'.
+Examples:
+6 -> "101010"
+4 -> "1010"
+12 -> "101010101010"
 
 The size will always be positive and will only use whole numbers.
 */
