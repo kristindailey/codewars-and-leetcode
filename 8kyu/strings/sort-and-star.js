46d0d424e6340f7be77e15e8cc95565e55c1cb77
Kristin Dailey <kristin.dailey02@gmail.com>
2024-02-13 12:00:00 -0800

    Added tests

--- 8kyu/strings/sort-and-star.js
+++ 8kyu/strings/sort-and-star.js
@@ -10,5 +10,9 @@ You should not remove or add elements from/to the array.
 // MY SOLUTION
 const twoSort = s => s.sort()[0].split("").join("***");
 
+console.log(twoSort(["Jan", "March", "Feb"]), "F***e***b***");
+console.log(twoSort(["Aloe", "Oil", "Lotion"]), "A***l***o***e***");
+console.log(twoSort(["wow", "ugh", "ew"]), "e***w***");
+
 // OTHER SOLUTION
 const two_Sort = s => [...s.sort()[0]].join("***");
