4783ba1047a670b93b50afe9ceaa5a9b185e8269
Kristin Dailey <kristin.dailey02@gmail.com>
2024-02-23 12:00:00 -0800

    Added tests

--- 8kyu/strings/reversed-strings.js
+++ 8kyu/strings/reversed-strings.js
@@ -10,3 +10,7 @@ Complete the solution so that it reverses the string passed into it.
 function reverseStr(str) {
     return str.split("").reverse().join("");
 }
+
+console.log(reverseStr("world"), "dlrow");
+console.log(reverseStr("word"), "drow");
+console.log(reverseStr("leon"), "noel");
