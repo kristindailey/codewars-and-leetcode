60b591d0a2fa7eede2327c0d40fc48909b944bbd
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-03 12:00:00 -0800

    Updated description

--- 8kyu/strings/remove-first-and-last-char-string.js
+++ 8kyu/strings/remove-first-and-last-char-string.js
@@ -1,5 +1,9 @@
-// CHALLENGE DESCRIPTION
-// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter: the original string. You don't have to worry about strings with less than two characters.
+/* CHALLENGE DESCRIPTION
+
+Your goal is to write a function that removes the first and last characters of a string. 
+
+You're given one parameter: the original string. You don't have to worry about strings with less than two characters.
+*/
 
 // MY SOLUTION
 function removeChar(str) {
