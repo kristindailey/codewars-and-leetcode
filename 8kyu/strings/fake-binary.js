d1869146b100bee8df1f0c8ed97c5f50b326c3a8
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-21 12:00:00 -0700

    Updated description

--- 8kyu/strings/fake-binary.js
+++ 8kyu/strings/fake-binary.js
@@ -1,6 +1,9 @@
-// CHALLENGE DESCRIPTION
-// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-// Note: Input will never be an empty string.
+/* CHALLENGE DESCRIPTION
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: The input will never be an empty string.
+*/
 
 // MY SOLUTION
 function fakeBin(x) {
