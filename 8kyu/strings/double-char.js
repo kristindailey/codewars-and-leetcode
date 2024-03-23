76b913fb78b0e4fb99b5250c85f861641dc3a97d
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-23 12:00:00 -0700

    Updated description

--- 8kyu/strings/double-char.js
+++ 8kyu/strings/double-char.js
@@ -1,11 +1,11 @@
 /* CHALLENGE DESCRIPTION
 
-Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Given a string, return a string in which each character (case-sensitive) is repeated once.
 
 Examples (Input -> Output):
-* "String"      -> "SSttrriinngg"
-* "Hello World" -> "HHeelllloo  WWoorrlldd"
-* "1234!_ "     -> "11223344!!__  "
+"String"      -> "SSttrriinngg"
+"Hello World" -> "HHeelllloo  WWoorrlldd"
+"1234!_ "     -> "11223344!!__  "
 */
 
 // MY SOLUTION
