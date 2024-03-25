45c2f372255b5ad519d8e2ff558dffc66ec05f49
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-25 12:00:00 -0700

    Added tests

--- 8kyu/strings/count-sheep-str.js
+++ 8kyu/strings/count-sheep-str.js
@@ -33,6 +33,10 @@ function countSheep(num) {
     return result;
 }
 
+console.log(countSheep(3), "1 sheep...2 sheep...3 sheep...");
+console.log(countSheep(0), "");
+console.log(countSheep(1), "1 sheep...");
+
 // OTHER SOLUTIONS
 const countSheep = num => Array.from({ length: num }, (_, i) => `${i + 1} sheep...`).join('');
 
