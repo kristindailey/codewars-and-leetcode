12d83d6fea52665254987c54c8bcd4e3e29ca349
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-28 12:00:00 -0700

    Updated description

--- 8kyu/strings/count-occurrences.js
+++ 8kyu/strings/count-occurrences.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+Write a function that accepts a string and a single character, and returns an integer of the count of occurrences the second argument is found in the first one.
 
 If no occurrences can be found, a count of 0 should be returned.
 
