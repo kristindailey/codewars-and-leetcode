20dd22120553b80bfe71b7bc14a4dd23227d9f8a
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-30 12:00:00 -0700

    Added tests

--- 8kyu/strings/convert-string-to-array.js
+++ 8kyu/strings/convert-string-to-array.js
@@ -10,3 +10,6 @@ Examples (Input ==> Output):
 
 // MY SOLUTION
 const stringToArray = string => string.split(" ");
+
+console.log(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+console.log(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
