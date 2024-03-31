dd9d86a6642fe22d7e884c54458fe9c1082e6ae0
Kristin Dailey <kristin.dailey02@gmail.com>
2024-03-31 12:00:00 -0700

    Added tests

--- 8kyu/strings/convert-number-to-string.js
+++ 8kyu/strings/convert-number-to-string.js
@@ -15,6 +15,10 @@ function numberToString(num) {
     return num.toString();
 }
 
+console.log(numberToString(123), "123");
+console.log(numberToString(999), "999");
+console.log(numberToString(-100), "-100");
+
 // OTHER SOLUTION
 const numberToString = num => num.toString();
 
