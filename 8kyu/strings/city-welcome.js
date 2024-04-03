9ef4880831eb5b1fb8ccbd5fec307390508cfb55
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-03 12:00:00 -0700

    Updated description

--- 8kyu/strings/city-welcome.js
+++ 8kyu/strings/city-welcome.js
@@ -1,11 +1,13 @@
 /* CHALLENGE DESCRIPTION
 
-Create a method that takes as input a name, city, and state to welcome a person. Note: The name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+Create a method that takes as input a name, city, and state to welcome a person. 
+
+Note: The name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 
 Example:
 ['John', 'Smith'], 'Phoenix', 'Arizona'
 
-This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+This example will return the string "Hello, John Smith! Welcome to Phoenix, Arizona!"
 */
 
 // function that takes in arr, string, string
