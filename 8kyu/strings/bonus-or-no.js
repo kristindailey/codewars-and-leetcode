34c1c528ef125e2f8363453be623a13c3c55b000
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-07 12:00:00 -0700

    Updated description

--- 8kyu/strings/bonus-or-no.js
+++ 8kyu/strings/bonus-or-no.js
@@ -2,9 +2,9 @@
 
 It's bonus time in the big city! The fat cats are rubbing their paws in anticipation. But who is going to make the most money?
 
-Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+Write a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus will be a boolean.
 
-If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fat cat did not make enough money and must receive only their stated salary.
 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Dart, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
 */
