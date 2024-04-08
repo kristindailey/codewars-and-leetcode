b0f9181d8ce56edb710ec69a2733a3bc8ad12dbe
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-08 12:00:00 -0700

    Added tests

--- 8kyu/strings/add-length.js
+++ 8kyu/strings/add-length.js
@@ -4,13 +4,16 @@ Your task is to write a function that takes a string and returns an array with t
 
 Examples: (Input --> Output)
 "apple ban" --> ["apple 5", "ban 3"]
-"you will win" -->["you 3", "will 4", "win 3"]
+"you will win" --> ["you 3", "will 4", "win 3"]
 
 Note: String will have at least one element. Words will always be separated by a space.
 */
 
 // MY SOLUTION
 const addLength = str => str.split(" ").map(word => word + " " + word.length);
+
+console.log(addLength("apple ban"), ["apple 5", "ban 3"]);
+console.log(addLength("you will win"), ["you 3", "will 4", "win 3"]);
 
 // OTHER SOLUTION
 const addL = str => str.split(" ").map(word => `${word} ${word.length}`);
