27a3452c710396529da013bda5ab6007f6f9569f
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-12 12:00:00 -0700

    Added tests

--- 8kyu/arrays/calculate-average.js
+++ 8kyu/arrays/calculate-average.js
@@ -1,4 +1,5 @@
 /* CHALLENGE DESCRIPTION
+
 Write a function which calculates the average of the numbers in a given list.
 
 Note: Empty arrays should return 0.
@@ -10,3 +11,7 @@ Note: Empty arrays should return 0.
 
 // MY SOLUTION
 const calculateAverage = arr => arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
+
+console.log(calculateAverage([90, 80, 70]), 80);
+console.log(calculateAverage([]), 0);
+console.log(calculateAverage([-10, 20, -30]), -6.66666666667);
