e1612461d207c76455501cc447b209a683feabc4
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-15 12:00:00 -0700

    Added tests

--- 8kyu/arrays/convert-boolean-to-string.js
+++ 8kyu/arrays/convert-boolean-to-string.js
@@ -5,6 +5,9 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 // MY SOLUTION
 const returnYesOrNo = boolean => boolean ? "Yes" : "No";
 
+console.log(returnYesOrNo(true), "Yes");
+console.log(returnYesOrNo(false), "No");
+
 // OTHER SOLUTION
 function convertBoolToWord(bool) {
   if (bool) {
