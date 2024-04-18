5ca769807b7ea1d84d8867f914ff8fc0d14937da
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-18 12:00:00 -0700

    Added tests

--- 8kyu/arrays/count-by-x.js
+++ 8kyu/arrays/count-by-x.js
@@ -15,12 +15,15 @@ countBy(2, 5) === [2, 4, 6, 8, 10]
 function countBy(x, n) {
   let result = [];
 
-  for (let i = 0; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     result.push(i * x);
   }
 
   return result;
 }
+
+console.log(countBy(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(countBy(2, 5), [2, 4, 6, 8, 10]);
 
 // OTHER SOLUTIONS
 const countBy = (x, n) => Array.from({length: n},(_,index) => x + index * x);
