bcb18aa2f51ff721d25af6867cbbe8ccbfd67181
Kristin Dailey <kristin.dailey02@gmail.com>
2024-04-19 12:00:00 -0700

    Added tests

--- 8kyu/arrays/count-positives-sum-negatives.js
+++ 8kyu/arrays/count-positives-sum-negatives.js
@@ -32,6 +32,9 @@ function countPositivesSumNegatives(input) {
   return [positiveCount, negativeSum];
 }
 
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]), [10, -65]);
+console.log(countPositivesSumNegatives([]), []);
+
 // OTHER SOLUTIONS
 function countPositivesSumNegatives(input) {
     return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
