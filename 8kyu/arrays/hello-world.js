af11e06aaa7c0f95c8e588de8eab1401ea010d7c
Kristin Dailey <kristin.dailey02@gmail.com>
2024-05-04 12:00:00 -0700

    Added tests

--- 8kyu/arrays/hello-world.js
+++ 8kyu/arrays/hello-world.js
@@ -1,10 +1,14 @@
-// CHALLENGE DESCRIPTION
-// Make a simple function called greet that returns the most-famous "hello world!" statement.
+/* CHALLENGE DESCRIPTION
+
+Make a simple function called greet that returns the most-famous "hello world!" statement.
+*/
 
 // MY SOLUTION
 function greet() {
     return "hello world!";
 }
+
+console.log(greet());
 
 // OTHER SOLUTION
 const greet = () => "hello world!";
