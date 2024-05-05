6c758a1d8c50d4eecc730e57e8cf9a2617f81855
Kristin Dailey <kristin.dailey02@gmail.com>
2024-05-05 12:00:00 -0700

    Updated description

--- 8kyu/arrays/wolf-in-sheeps-clothing.js
+++ 8kyu/arrays/wolf-in-sheeps-clothing.js
@@ -6,7 +6,8 @@ Warn the sheep in front of the wolf that it is about to be eaten. Remember that 
 
 [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
    7      6      5      4      3            2      1
-If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep." Otherwise, return "Oi! Sheep number ${N}! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
 
 Note: There will always be exactly one wolf in the array.
 
