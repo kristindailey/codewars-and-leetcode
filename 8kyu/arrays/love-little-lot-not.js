027c38bda8830be25c274c92a8d747b07e532b3c
Kristin Dailey <kristin.dailey02@gmail.com>
2024-05-08 12:00:00 -0700

    Added tests

--- 8kyu/arrays/love-little-lot-not.js
+++ 8kyu/arrays/love-little-lot-not.js
@@ -31,6 +31,11 @@ function howMuchILoveYou(nbPetals) {
   
   return phrases[index];
 }
+
+console.log(howMuchILoveYou(7), "I love you");
+console.log(howMuchILoveYou(4), "passionately");
+console.log(howMuchILoveYou(6), "not at all");
+
 // OTHER SOLUTIONS
 const howMuchILoveYou = n => ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][(n - 1) % 6];
 
