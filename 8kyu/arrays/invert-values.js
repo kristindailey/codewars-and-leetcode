d483e89f690df408fc3919f8a52b6d7db6b52505
Kristin Dailey <kristin.dailey02@gmail.com>
2024-05-11 12:00:00 -0700

    Added tests

--- 8kyu/arrays/invert-values.js
+++ 8kyu/arrays/invert-values.js
@@ -14,5 +14,9 @@ function invert(array) {
     return array.map(num => -num);
 }
 
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]); 
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]); 
+console.log(invert([]), []); 
+
 // OTHER SOLUTION
 const invert = array => array.map(num => -num);
