9b24c90f5ee4c37723d5c6da1d3cd595fbb65ac7
Kristin Dailey <kristin.dailey02@gmail.com>
2024-05-22 12:00:00 -0700

    Updated solutions

--- 8kyu/arrays/sum-of-positive.js
+++ 8kyu/arrays/sum-of-positive.js
@@ -10,7 +10,6 @@ Note: If there is nothing to sum, the sum should default to 0.
 // MY SOLUTION
 function positiveSum(arr) {
     let sum = 0;
-
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
             sum += arr[i];
@@ -22,4 +21,4 @@ function positiveSum(arr) {
 // OTHER SOLUTIONS
 const positiveSum = arr => arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 
-const positiveSum = arr => arr.filter(x => x > 0).reduce((a, b) => a + b, 0);
+const positiveSum = arr => arr.filter(num => num > 0).reduce((a, b) => a + b, 0);
