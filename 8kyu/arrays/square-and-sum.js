97d47fd85d205c746dee3cfb2e86fe43a7d6b610
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-05 12:00:00 -0700

    Updated description

--- 8kyu/arrays/square-and-sum.js
+++ 8kyu/arrays/square-and-sum.js
@@ -1,5 +1,8 @@
-// CHALLENGE DESCRIPTION
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+/* CHALLENGE DESCRIPTION
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+*/
 
 // MY SOLUTION
 function squareSum(numbers) {
