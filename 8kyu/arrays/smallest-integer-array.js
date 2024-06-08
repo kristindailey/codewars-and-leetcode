8c5d6a50e00be3762a82d812217efba57cd84198
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-08 12:00:00 -0700

    Updated description

--- 8kyu/arrays/smallest-integer-array.js
+++ 8kyu/arrays/smallest-integer-array.js
@@ -1,13 +1,12 @@
-/*
-CHALLENGE DESCRIPTION
+/* CHALLENGE DESCRIPTION
 
-Given an array of integers your solution should find the smallest integer.
+Given an array of integers, your solution should find the smallest integer.
 
 For example:
+Given [34, 15, 88, 2], your solution should return 2.
+Given [34, -345, -1, 100], your solution should return -345.
 
-Given [34, 15, 88, 2] your solution will return 2
-Given [34, -345, -1, 100] your solution will return -345
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
+You can assume for the purpose of this kata that the supplied array will not be empty.
 */
 
 // MY SOLUTION
