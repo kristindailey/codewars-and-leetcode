eacf082c7bc29b5c6f92ebb04a715dced4421402
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-09 12:00:00 -0700

    Added tests

--- 8kyu/arrays/simple-multiplication.js
+++ 8kyu/arrays/simple-multiplication.js
@@ -13,5 +13,9 @@ function simpleMultiplication(number) {
     }
 }
 
+console.log(simpleMultiplication(2), 16);
+console.log(simpleMultiplication(3), 27);
+console.log(simpleMultiplication(1), 9);
+
 // OTHER SOLUTION
 const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9;
