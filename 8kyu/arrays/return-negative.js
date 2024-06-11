ad3966e7d35bb033aad44218b6ded42147d4676d
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-11 12:00:00 -0700

    Added tests

--- 8kyu/arrays/return-negative.js
+++ 8kyu/arrays/return-negative.js
@@ -15,12 +15,17 @@ Zero (0) is not checked for any specific sign. Negative zeros make no mathematic
 
 // MY SOLUTION
 function makeNegative(num) {
-    if (num < 0) {
+    if (num <= 0) {
         return num;
     } else {
         return -num;
     }
 }
+
+console.log(makeNegative(1), -1);
+console.log(makeNegative(-5), -5);
+console.log(makeNegative(0), 0);
+console.log(makeNegative(0.12), -0.12);
 
 // OTHER SOLUTIONS
 const makeNegative = num => num > 0 ? -num : num;
