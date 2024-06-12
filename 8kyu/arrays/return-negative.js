2cdc6293c77cef35db237b26e9e20ddb6d2742ae
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-12 12:00:00 -0700

    Updated solution

--- 8kyu/arrays/return-negative.js
+++ 8kyu/arrays/return-negative.js
@@ -13,7 +13,6 @@ The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 */
 
-
 // MY SOLUTION
 function makeNegative(num) {
     if (num < 0) {
