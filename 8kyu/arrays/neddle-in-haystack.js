1a6177ab7d54ff7d813428a4e38d77989997ee89
Kristin Dailey <kristin.dailey02@gmail.com>
2024-06-27 12:00:00 -0700

    Updated description

--- 8kyu/arrays/neddle-in-haystack.js
+++ 8kyu/arrays/neddle-in-haystack.js
@@ -1,17 +1,16 @@
 /* CHALLENGE DESCRIPTION
+
 Can you find the needle in the haystack?
 
-Write a function findNeedle() that takes an array full of junk but containing one "needle"
+Write a function findNeedle() that takes an array full of junk but containing one "needle."
 
-After your function finds the needle it should return a message (as a string) that says:
+After your function finds the needle, it should return a message (as a string) that says the following:
 
-"found the needle at position " plus the index it found the needle, so:
+`found the needle at position ${index}`
 
-Example(Input --> Output)
+Example (Input --> Output):
 
 ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
-
-Note: In COBOL, it should return "found the needle at position 6"
 */
 
 // MY SOLUTION
