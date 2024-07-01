35248e16b9703bf786d88f21805ec796cefa0add
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-01 12:00:00 -0700

    Updated solution

--- 8kyu/arrays/love-little-lot-not.js
+++ 8kyu/arrays/love-little-lot-not.js
@@ -1,4 +1,5 @@
 /* CHALLENGE DESCRIPTION
+
 Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
 
 "I love you"
@@ -7,9 +8,10 @@ Who remembers back to their time in the schoolyard, when girls would take a flow
 "passionately"
 "madly"
 "not at all"
-If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
 
-When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+If there are more than 6 petals, you start over with "I love you." For 7 petals, "a little." And so on.
+
+When the last petal was torn, there were cries of excitement, dreams, surging thoughts and emotions.
 
 Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
 */
@@ -32,15 +34,15 @@ function howMuchILoveYou(nbPetals) {
 // OTHER SOLUTIONS
 const howMuchILoveYou = n => ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][(n - 1) % 6];
 
-const phrases = [
+function howMuchILoveYou(n) {
+  const phrases = [
     'I love you',
     'a little',
     'a lot',
     'passionately',
     'madly',
     'not at all',
-]
+  ];
 
-function howMuchILoveYou(n) {
-     return phrases[(n - 1) % phrases.length] 
+  return phrases[(n - 1) % phrases.length];
 }
