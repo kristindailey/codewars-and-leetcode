62abf1034a1345558b41af23437747d7750937a2
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-03 12:00:00 -0700

    Updated description

--- 8kyu/arrays/keep-hydrated.js
+++ 8kyu/arrays/keep-hydrated.js
@@ -7,7 +7,6 @@ Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of w
 You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
 
 For example:
-
 time = 3 ----> litres = 1
 
 time = 6.7---> litres = 3
