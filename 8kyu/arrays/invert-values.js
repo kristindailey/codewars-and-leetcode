fe6c816dbaab13293a6d9d9295dde2e126416553
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-04 12:00:00 -0700

    Updated description

--- 8kyu/arrays/invert-values.js
+++ 8kyu/arrays/invert-values.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+Given a set of numbers, return the additive inverse of each. Each positive becomes negative, and the negatives become positives.
 
 [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
 [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
@@ -14,5 +14,5 @@ function invert(array) {
     return array.map(num => -num);
 }
 
-// OTHER SOLUTIONS
+// OTHER SOLUTION
 const invert = array => array.map(num => -num);
