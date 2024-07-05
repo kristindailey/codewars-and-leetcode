908f519eba50989865c3f28e28a6318ce205b499
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-05 12:00:00 -0700

    Updated solution

--- 8kyu/arrays/hello-world.js
+++ 8kyu/arrays/hello-world.js
@@ -1,7 +1,10 @@
 // CHALLENGE DESCRIPTION
-// Make a simple function called greet that returns the most-famous "hello world!".
+// Make a simple function called greet that returns the most-famous "hello world!" statement.
 
 // MY SOLUTION
 function greet() {
     return "hello world!";
 }
+
+// OTHER SOLUTION
+const greet = () => "hello world!";
