11c1ffc427bb4f3dfdcd49192beecfdd28fc4986
daileycoder <159469428+daileycoder@users.noreply.github.com>
2024-07-15 14:27:34 -0700

    codewars update

---
+++ 8kyu/multiply.js
@@ -0,0 +1,10 @@
+// CHALLENGE DESCRIPTION
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b) {
+//     a * b;
+// }
+
+// MY SOLUTION
+function multiply(a, b) {
+    return a * b;
+  }
