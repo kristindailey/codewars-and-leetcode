27ed3a82cbba820e296bb222171b34e2dadee46c
daileycoder <159469428+daileycoder@users.noreply.github.com>
2024-07-21 17:27:54 -0700

    codewars update

---
+++ 8kyu/code-under-pressure-one.js
@@ -0,0 +1,10 @@
+// CHALLENGE DESCRIPTION
+// Code as fast as you can! You need to double the integer and return it.
+
+// MY SOLUTION
+function doubleInteger(i) {
+    return i * 2;
+}
+
+// OTHER SOLUTIONS
+const doubleInteger = i => 2 * i;
---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,18 @@
+// CHALLENGE DESCRIPTION
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+// MY SOLUTION
+function problem(x) {
+    if (typeof x === "string") {
+      return "Error";
+    } else {
+      return (x * 50) + 6;
+    }
+}
+
+// OTHER SOLUTIONS
+const problem = x => typeof x === "string" ? "Error" : x * 50 + 6;
+
+function problem(x) {
+    return typeof x === "number" ? x * 50 + 6 : "Error";
+}
