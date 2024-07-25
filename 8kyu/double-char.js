fcd9facd7ae895f431c277e21782b0e45a16db39
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-24 18:35:04 -0700

    codewars update

--- 8kyu/double-char.js
+++ 8kyu/double-char.js
@@ -1,5 +1,5 @@
-// CHALLENGE DESCRIPTION
-/*
+/* CHALLENGE DESCRIPTION
+
 Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
 Examples (Input -> Output):
