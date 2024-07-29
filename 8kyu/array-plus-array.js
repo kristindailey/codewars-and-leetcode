d329415b17345c54da9ab7039afffb7fab51dc3b
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-28 19:08:27 -0700

    codewars update

---
+++ 8kyu/array-plus-array.js
@@ -0,0 +1,24 @@
+// CHALLENGE DESCRIPTION
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+// MY SOLUTION
+function arrayPlusArray(arr1, arr2) {
+    let sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      sum += arr1[i];
+    }
+    for (let j = 0; j < arr2.length; j++) {
+      sum += arr2[j];
+    }
+    return sum;
+}
+
+// OTHER SOLUTIONS
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((a, b) => a + b, 0);
+
+const arrayPlusArray = (arr1, arr2) => arr1.reduce((a, b) => a + b) + arr2.reduce((a, b) => a + b);
---
+++ 8kyu/removing-elements.js
@@ -0,0 +1,43 @@
+/* CHALLENGE DESCRIPTION
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+// MY SOLUTION
+function removeEveryOther(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (i % 2 === 1) { 
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
+}
+
+// OTHER SOLUTIONS
+const removeEveryOther = arr => arr.filter((_, i) => !(i % 2));
+
+function removeEveryOther(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        arr.splice(i, 1);
+    }
+    return arr;
+}
+
+function removeEveryOther(arr) {
+    let newArr = [];
+    for (var i = 0; i < arr.length; i += 2) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+function removeEveryOther(arr) {
+    return arr.filter(function(elem, index) {
+      return index % 2 === 0;
+    });
+}
