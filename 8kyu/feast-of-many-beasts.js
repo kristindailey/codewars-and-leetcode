9d2d11af18116bdf67a003961b1afe0556c8591b
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-31 15:53:55 -0700

    codewars update

--- 8kyu/feast-of-many-beasts.js
+++ 8kyu/feast-of-many-beasts.js
@@ -1,5 +1,4 @@
-// CHALLENGE DESCRIPTION
-/*
+/* CHALLENGE DESCRIPTION
 All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
 
 Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
@@ -22,10 +21,6 @@ function feast(beast, dish) {
 }
 
 // OTHER SOLUTIONS
-function feast(beast, dish) {
-	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1];
-}
+const feast = (beast, dish) => beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1];
 
-function feast(beast, dish) {
-    return beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1);
-}
+const feast = (beast, dish) => beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1);
