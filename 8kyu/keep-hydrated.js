8d5734340173514dc430c7938a650dcdb24165f4
Kristin Dailey <kristin.dailey02@gmail.com>
2024-07-31 16:12:58 -0700

    codewars update

--- 8kyu/keep-hydrated.js
+++ 8kyu/keep-hydrated.js
@@ -21,8 +21,6 @@ function litres(time) {
 }
 
 // OTHER SOLUTIONS
-function litres(time) {
-    return Math.floor(time / 2);
-}
+const litres = time => Math.floor(time / 2);
 
 const litres = time => Math.floor(time * 0.5);
