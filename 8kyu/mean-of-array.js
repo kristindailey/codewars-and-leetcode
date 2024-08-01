323499b6b7d3fa6e3f3260c466c6061e43619f96
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-01 09:08:26 -0700

    codewars update

--- 8kyu/mean-of-array.js
+++ 8kyu/mean-of-array.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy! You just need to write a script.
 
 Return the average of the given array rounded down to its nearest integer.
 
