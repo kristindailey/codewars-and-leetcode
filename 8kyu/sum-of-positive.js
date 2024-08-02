967198d5ac53fb94b1affb2e3702440c749b0706
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-02 10:36:31 -0700

    codewars update

--- 8kyu/sum-of-positive.js
+++ 8kyu/sum-of-positive.js
@@ -1,9 +1,10 @@
-// CHALLENGE DESCRIPTION
-/* You get an array of numbers, return the sum of all of the positives ones.
+/* CHALLENGE DESCRIPTION
 
-Example [1,-4,7,12] => 1 + 7 + 12 = 20
+You get an array of numbers. Return the sum of all of the positive ones.
 
-Note: if there is nothing to sum, the sum is default to 0.
+Example: [1, -4, 7, 12] => 1 + 7 + 12 = 20
+
+Note: If there is nothing to sum, the sum is default to 0.
 */
 
 // MY SOLUTION
@@ -19,10 +20,6 @@ function positiveSum(arr) {
 }
 
 // OTHER SOLUTIONS
-function positiveSum(arr) {
-    return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
- }
+const positiveSum = arr => arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 
- function positiveSum(arr) {
-    return arr.filter(x => x > 0).reduce((a, b) => a + b, 0);
-  }
+const positiveSum = arr => arr.filter(x => x > 0).reduce((a, b) => a + b, 0);
