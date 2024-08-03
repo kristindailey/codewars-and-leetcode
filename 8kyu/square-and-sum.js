d1d2e854678e6ec2918ebc7d7fd80a0d59bd651c
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-03 10:35:25 -0700

    codewars update

--- 8kyu/square-and-sum.js
+++ 8kyu/square-and-sum.js
@@ -11,12 +11,10 @@ function squareSum(numbers) {
 }
 
 // OTHER SOLUTIONS
+const squareSum = numbers => numbers.reduce((sum, num) => sum + (num * num), 0);
+
 function squareSum(numbers) {
     return numbers.reduce(function(sum, n) {
       return (n * n) + sum;
     }, 0);
-}
-
-function squareSum(numbers) {
-    return numbers.reduce((sum,num) => sum + (num * num), 0);
 }
