620c17caa7a41b3325d09f0a748ae33c01202b19
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-05 09:57:36 -0700

    codewars update

---
+++ 8kyu/head-at-wrong-end.js
@@ -0,0 +1,16 @@
+/* CHALLENGE DESCRIPTION
+
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong. Someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logic.
+*/
+
+// MY SOLUTION
+const fixTheMeerkat = arr => [arr[2], arr[1], arr[0]];
+
+// OTHER SOLUTIONS
+const fixMeer = arr => arr.reverse();
+
+const fixMeerkat = ([tail, body, head]) => [head, body, tail];
