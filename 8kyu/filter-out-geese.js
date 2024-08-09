6dc350c54258adc679207836f040c42f28a1502a
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-09 08:39:35 -0700

    codewars update

--- 8kyu/filter-out-geese.js
+++ 8kyu/filter-out-geese.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the "geese" removed.
 
 The geese are any strings in the following array, which is pre-populated in your solution:
 ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
@@ -11,7 +11,7 @@ For example, if this array were passed as an argument:
 Your function would return the following array:
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 
-The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the "geese" removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 */
 
 // MY SOLUTION
