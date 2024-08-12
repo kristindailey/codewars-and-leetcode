7fc999bba150bcc1afd266caac5917feb6dde5a2
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-12 08:39:04 -0700

    codewars update

--- 8kyu/sort-and-star.js
+++ 8kyu/sort-and-star.js
@@ -1,8 +1,8 @@
 /* CHALLENGE DESCRIPTION
 
-You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+You will be given a list of strings. You must sort it alphabetically (case-sensitive and based on the ASCII values of the chars) and then return the first value.
 
-The returned value must be a string, and have "***" between each of its letters.
+The returned value must be a string and have "***" between each of its letters.
 
 You should not remove or add elements from/to the array.
 */
