c0cf6cf625e18eeca23e837968e9cba295ea7022
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-12 08:44:44 -0700

    codewars update

--- 8kyu/find-multiples-nums.js
+++ 8kyu/find-multiples-nums.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-In this simple exercise, you will build a program that takes a value, integer, and returns a list of its multiples up to another value, limit. 
+In this simple exercise, you will build a program that takes a value, integer, and returns a list of its multiples up to another value limit. 
 
 If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. 
 
--- 8kyu/find-nums-divisible.js
+++ 8kyu/find-nums-divisible.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. The first argument is an array of numbers and the second is the divisor.
 
 Example(Input1, Input2 --> Output)
 [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
