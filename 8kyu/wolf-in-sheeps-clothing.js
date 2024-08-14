7ef7d8843b7aefc012735e52d0696015ad9dc3d5
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-14 10:36:53 -0700

    codewars update

--- 8kyu/wolf-in-sheeps-clothing.js
+++ 8kyu/wolf-in-sheeps-clothing.js
@@ -2,7 +2,7 @@
 
 Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
 
-Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array.
 
 [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
    7      6      5      4      3            2      1
