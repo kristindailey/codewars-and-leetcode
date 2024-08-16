ac224edc053c79827f2fb9bda18402fb00294605
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-16 08:39:49 -0700

    codewars update

--- 8kyu/add-length.js
+++ 8kyu/add-length.js
@@ -1,14 +1,14 @@
 /* CHALLENGE DESCRIPTION
 
-What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+We need the length of the words separated by a space to be added at the end of that same word and have it returned as an array.
 
-Example(Input --> Output)
-
+Examples: (Input --> Output)
 "apple ban" --> ["apple 5", "ban 3"]
 "you will win" -->["you 3", "will 4", "win 3"]
-Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element.
 
-Note: String will have at least one element; words will always be separated by a space.
+Your task is to write a function that takes a string and returns an array with the length of each word added to each element.
+
+Note: String will have at least one element. Words will always be separated by a space.
 */
 
 // MY SOLUTION
