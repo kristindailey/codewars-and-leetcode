9c0f6b98922f996f55b31899f45eb7a8043dad66
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-18 08:24:47 -0700

    codewars update

--- 8kyu/well-of-ideas.js
+++ 8kyu/well-of-ideas.js
@@ -1,10 +1,10 @@
 /* CHALLENGE DESCRIPTION
 
-For every good kata idea there seem to be quite a few bad ones!
+For every good kata idea there seem to be quite a few bad ones.
 
-In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
-If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
-If there are no good ideas, as is often the case, return 'Fail!'.
+In this kata you need to check the provided array (x) for good ideas "good" and bad ideas "bad". 
+If there are one or two good ideas, return "Publish!". If there are more than two, return "I smell a series!". 
+If there are no good ideas, as is often the case, return "Fail!".
 */
 
 // MY SOLUTION
