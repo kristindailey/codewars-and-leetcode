a162fcea4c9b5f1647766b51ff96a8b81948b1c4
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-25 09:44:20 -0700

    codewars update

--- 7kyu/odd-or-even.js
+++ 7kyu/odd-or-even.js
@@ -4,7 +4,7 @@ Given a list of integers, determine whether the sum of its elements is odd or ev
 
 Give your answer as a string matching "odd" or "even".
 
-If the input array is empty consider it as: [0] (array with a zero).
+If the input array is empty, consider it as the following: [0] (array with a zero).
 
 Examples:
 Input: [0]
