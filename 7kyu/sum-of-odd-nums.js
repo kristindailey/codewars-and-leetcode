e81508f4fb5cda66dc054356bae1e98a8d303671
Kristin Dailey <kristin.dailey02@gmail.com>
2024-08-25 09:28:32 -0700

    codewars update

--- 7kyu/sum-of-odd-nums.js
+++ 7kyu/sum-of-odd-nums.js
@@ -1,21 +1,21 @@
 /* CHALLENGE DESCRIPTION
 
-Given the triangle of consecutive odd numbers:
+Given the triangle of consecutive odd numbers, calculate the sum of the numbers in the nth row of this triangle (starting at index 1).
+
 
              1
           3     5
        7     9    11
    13    15    17    19
 21    23    25    27    29
-...
-Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 
+Examples: (Input --> Output)
 1 -->  1
 2 --> 3 + 5 = 8
 */
 
 // MY SOLUTION
-const rowSumOddNumbers = n => n * n * n;
+const rowSumOddNumbers = n => n ** 3;
 
 // OTHER SOLUTION
 const rowSumOddNums = n => Math.pow(n, 3);
