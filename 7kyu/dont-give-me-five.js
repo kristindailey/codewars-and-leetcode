f54c8a6d6c549b83475215c00b05aca1a36fbc48
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-01 09:03:37 -0700

    codewars update

--- 7kyu/dont-give-me-five.js
+++ 7kyu/dont-give-me-five.js
@@ -1,12 +1,14 @@
 /* CHALLENGE DESCRIPTION
 
-In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+In this kata you get the start number and the end number and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive.
+
+Note: 
+The result may contain fives.
+The start number will always be smaller than the end number. Both numbers can be also negative.
 
 Examples:
-1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
-4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
-The result may contain fives.
-The start number will always be smaller than the end number. Both numbers can be also negative!
+1, 9 -> 1, 2, 3, 4, 6, 7, 8, 9 -> Result 8
+4, 17 -> 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17 -> Result 12
 */
 
 /* PREP
@@ -42,7 +44,7 @@ function dontGiveMeFive(start, end) {
 function dontGiveMeFive(start, end) {
     let res = [];
     for (let i = start; i <= end; i++) {
-      if (!i.toString().includes('5')) res.push(i);
+      if (!i.toString().includes("5")) res.push(i);
     }
     return res.length;
 }
@@ -50,7 +52,7 @@ function dontGiveMeFive(start, end) {
 function dontGiveMeFive(start, end) {
     let count = 0;
     for (let i = start; i <= end; i++) {
-        if (!i.toString().includes('5')) count++;
+        if (!i.toString().includes("5")) count++;
     }
     return count;
 }
