aa14139093b47e3f667db2198b2e4282b7f63afa
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-03 08:36:19 -0700

    refactored my checkExam function

--- 7kyu/check-the-exam.js
+++ 7kyu/check-the-exam.js
@@ -45,13 +45,9 @@ Pseudocode
 function checkExam(array1, array2) {
     let score = 0;
     for (let i = 0; i < array1.length; i++) {
-        if (array2[i] === "") {
-            score += 0;
-        } else if (array2[i] === array1[i]) {
-            score += 4;
-        } else {
-            score -= 1;
-        }
+        if (array2[i] === "") score += 0;
+        else if (array2[i] === array1[i]) score += 4;
+        else score -= 1;
     }
     return score < 0 ? 0 : score;
 }
