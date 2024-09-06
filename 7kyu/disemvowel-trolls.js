ff288f52f773dc19e53e1387245cc793c367f761
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-06 08:55:00 -0700

    added disemvowel-trolls.js

---
+++ 7kyu/disemvowel-trolls.js
@@ -0,0 +1,37 @@
+/* CHALLENGE DESCRIPTION (STRING LADDER)
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+/* PREP
+Parameters
+    // string
+    // string may include uppercase and lowercase
+Returns 
+    // return new string with no vowels
+    // y is not considered a vowel
+Examples 
+    // "That is crazy, silly"
+        // "Tht s crzy slly"
+    //  "Hello world"
+        // "Hll wrld"
+Pseudocode 
+    // function that takes in a string
+*/
+
+// MY SOLUTION
+const returnNoVowels = str => Array.from(str).filter(letter => !("aeiouAEIOU".includes(letter))).join("");
+
+// OTHER SOLUTION
+function disemvowel(str) {
+    const vowels = 'aeiou';
+    return str.split('').filter(letter => !vowels.includes(letter.toLowerCase())).join('');
+}
