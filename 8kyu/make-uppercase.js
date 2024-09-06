34c65d141331a86c0db70b2a2277a9c5b52f25e4
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-06 08:09:09 -0700

    added make-uppercase.js

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,24 @@
+/* CHALLENGE DESCRIPTION (STRING LADDER)
+
+Write a function which converts the input string to uppercase.
+
+*/
+
+/* PREP
+Parameters
+    // string
+Returns 
+    // uppercase string
+Examples 
+    // "Hello"
+        // "HELLO"
+    //  "mYnAmEiS"
+        // "MYNAMEIS"
+Pseudocode 
+    // function that takes in a string
+    // attach method that turns string to uppercase
+    // return uppercase string
+*/
+
+// MY SOLUTION
+const returnUpperCaseStr = str => str.toUpperCase();
