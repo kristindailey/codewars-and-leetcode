57cff5f798dd80d038c4895f8fab67806f274f09
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-08 15:33:34 -0700

    codewars update

--- 8kyu/array-mean.js
+++ 8kyu/array-mean.js
@@ -6,7 +6,7 @@ To find the mean (average) of a set of numbers add all of the numbers together a
 
 For an example list of 1, 3, 5, 7:
 
-1. Add all of the numbers
+1. Add all of the numbers.
 1 + 3 + 5 + 7 = 16
 
 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
--- 8kyu/square-root-or-not.js
+++ 8kyu/square-root-or-not.js
@@ -1,16 +1,16 @@
 /* CHALLENGE DESCRIPTION (ARRAY LADDER)
 
-Write a method that will get an integer array as parameter and will process every number from this array.
+Write a method that will get an integer array as a parameter and will process every number from this array.
 
-Return a new array with processing every number of the input-array like this:
+Return a new array with processing every number of the input array like this:
 
-If the number has an integer square root, take this, otherwise square the number.
+If the number has an integer square root, take this. Otherwise, square the number.
 
 Example:
 [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 
 Notes:
-The input array will always contain only positive numbers, and will never be empty or null.
+The input array will always contain only positive numbers and will never be empty or null.
 */
 
 /* PREP
