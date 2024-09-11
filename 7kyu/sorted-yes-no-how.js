5edd5c76195ea86e9599fb3860c24898efb3e439
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-11 09:15:51 -0700

    updated sorted-yes-no-how.js

--- 7kyu/sorted-yes-no-how.js
+++ 7kyu/sorted-yes-no-how.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-Complete the method which accepts an array of integers, and returns one of the following:
+Complete the method which accepts an array of integers nd returns one of the following:
 
 "yes, ascending" - if the numbers in the array are sorted in an ascending order
 
