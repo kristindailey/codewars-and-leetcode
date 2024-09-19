15739bd7c4759ca7442600074d8cfd14f1347cbb
Kristin Dailey <kristin.dailey02@gmail.com>
2024-09-19 08:34:22 -0700

    added sort-arrays-1.js

---
+++ 7kyu/sort-arrays-1.js
@@ -0,0 +1,24 @@
+/* CHALLENGE DESCRIPTION
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+*/
+
+/* PREP
+Parameters
+    // array
+Returns 
+    // return array in lexicographical order
+Examples g
+    // ["hello", "apple", "carrot", "juice"]
+        // ["apple", "carrot", "hello", "juice"]
+    //  [10, 5, 9, 7, 6, 2]
+        // [10, 2, 5, 6, 7, 9]
+Pseudocode 
+    // function that takes in one parameter, array
+    // sort the array
+    // return the sorted array
+*/
+
+// MY SOLUTION
+const sortme = names => names.sort();
