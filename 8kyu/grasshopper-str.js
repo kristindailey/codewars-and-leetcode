db08d650122d2958a1c6880c454e72d85de5783e
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-08 08:17:07 -0700

    added grasshopper-str.js

---
+++ 8kyu/grasshopper-str.js
@@ -0,0 +1,33 @@
+/* CHALLENGE DESCRIPTION
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	                return
+name equals owner	    'Hello boss'
+otherwise	            'Hello guest'
+*/
+
+/* PREP
+Parameters
+    // two parameters, name and owner
+Returns 
+    // "Hello boss" if name === owner
+    // else "Hello guest"
+Examples 
+    // ("Julie", "Robert")
+        // "Hello guest"
+    //  ("Julie", "Julie")
+        // "Hello boss"
+Pseudocode 
+    // function that takes in two parameters, name and owner (strings)
+    // if name === owner return "Hello boss"
+    // else return "Hello guest"
+*/
+
+// MY SOLUTION
+const returnGreeting = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
+
+// OTHER SOLUTIONS
+const greet = (name, owner) => `Hello ${name === owner ? 'boss' : 'guest'}`;
