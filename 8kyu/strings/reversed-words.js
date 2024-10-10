5f8646273b07c1613bc50cf89acda8db89af0d44
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-10 12:00:00 -0700

    added reversed-words.js

---
+++ 8kyu/strings/reversed-words.js
@@ -0,0 +1,37 @@
+/* CHALLENGE DESCRIPTION
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+*/
+
+/* PREP
+PARAMETERS
+    // string
+    // no leading or trailing spaces
+RETURNS
+    // return the string with the words in reverse order
+EXAMPLES
+    // "Do or do not there is no try"
+        // "try no is there not do or Do"
+    // "The one who wins is the one who thinks she can"
+        // "can she thinks who one the is wins who one The"
+PSUEDOCODE
+    // function that takes one parameter, string
+    // turn string into array
+    // reverse the array
+    // join the strings together
+    // return the reversed string
+*/
+
+// MY SOLUTION
+function reverseWords(str) {
+    const arr = str.split(" ");
+    return arr.reverse().join(" ");
+}
+
+// OTHER SOLUTIONS
+const reverseWords = str => str.split(" ").reverse().join(" ");
