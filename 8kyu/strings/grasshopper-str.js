7c0873a2cc0711e0f25ec9546c9b17d65db9dab1
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-14 12:28:30 -0700

    updated grasshopper-str.js

--- 8kyu/strings/grasshopper-str.js
+++ 8kyu/strings/grasshopper-str.js
@@ -4,9 +4,9 @@ Create a function that gives a personalized greeting. This function takes two pa
 
 Use conditionals to return the proper message:
 
-case	                return
-name equals owner	    'Hello boss'
-otherwise	            'Hello guest'
+Case	                 Return
+name equals owner	    "Hello boss"
+otherwise	            "Hello guest"
 */
 
 /* PREP
