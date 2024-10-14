6b9b3521334f887266c4a9719e4b692f581eef8d
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-14 12:18:40 -0700

    updated count-sheep-str.js

--- 8kyu/strings/count-sheep-str.js
+++ 8kyu/strings/count-sheep-str.js
@@ -1,9 +1,11 @@
 /* CHALLENGE DESCRIPTION
 
-If you can't sleep, just count sheep!!
+If you can't sleep, just count sheep!
 
 Task:
-Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+
+The input will always be valid (i.e., no negative integers).
 */
 
 /* PREP
@@ -30,14 +32,6 @@ function countSheep(num) {
     let result = "";
     for (let i = 1; i <= num; i++) {
       result += `${i} sheep...`;
-    }
-    return result;
-}
-
-function countSheep(num) {
-    let result = "";
-    for (let i = 1; i <= num; i++) {
-        result += `${i} sheep...`;
     }
     return result;
 }
