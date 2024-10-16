7bfaf10954919f4315cacc6fd8a325190efe08a5
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-16 13:51:43 -0700

    updated mean-of-array.js

--- 8kyu/arrays/mean-of-array.js
+++ 8kyu/arrays/mean-of-array.js
@@ -7,6 +7,26 @@ Return the average of the given array rounded down to its nearest integer.
 The array will never be empty.
 */
 
+/* PREP
+Parameters
+    // arr, never empty
+Returns
+    // average of array, rounded down to nearest integer
+Examples
+    // [100, 50, 88, 90]
+        // 82
+    // [65, 67, 74, 80]
+        // 71
+    // [100, 99, 95, 88]
+        // 95
+Psuedocode
+    // function that takes in one param, arr
+    // sum all elements of the array
+    // divide by the length of the array
+    // apply Math.floor() to the average
+    // return the average
+*/
+
 // MY SOLUTION
 const getAverage = marks => Math.floor(marks.reduce((a, b) => a + b, 0) / marks.length);         
 
