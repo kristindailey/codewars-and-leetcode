60d1b41ec7eb6ee070138c900a0abb55620e7c1d
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-21 15:43:15 -0700

    finished arr.combinations.js kata

--- 6kyu/arrays/arr-combinations.js
+++ 6kyu/arrays/arr-combinations.js
@@ -13,18 +13,16 @@ Make sure that you don't count duplicates. For example, solve([[1, 2],[4, 4],[5,
 // array of arrays
 // return num of unique arrays, do not count dupes
 
-// MY SOLUTION -- IN PROGRESS
+// MY SOLUTION
 function returnUniqueArrs(arr) {
-    // get rid of dupes using set
-    const noDupes = new Set(arr);
-    // make a new arr using arr1[0], arr2[0], arr3[0]
-    // count sum of unique arrs
-
+    // get rid of dupes, set
+    // multiply length of sub arr, reduce
+    // return result of multiplication
+    return arr.reduce((a, c) => a * new Set(c).size, 1);
 }
 
-
-returnUniqueArrs([1, 3, 4], [4], [1, 3], 6);  
-returnUniqueArrs([1, 2], [4, 4], [5, 6, 6], 4)
-returnUniqueArrs([1, 2], [5, 5], [6, 7, 7], 4)
+console.log(returnUniqueArrs([1, 3, 4], [4], [1, 3], 6));  
+console.log(returnUniqueArrs([1, 2], [4, 4], [5, 6, 6], 4));
+console.log(returnUniqueArrs([1, 2], [5, 5], [6, 7, 7], 4));
 
 // OTHER SOLUTIONS
