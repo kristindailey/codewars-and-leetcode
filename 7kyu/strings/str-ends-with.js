cbb32782222b70e2c7598c703ca1029aac54c99a
Kristin Dailey <kristin.dailey02@gmail.com>
2024-11-02 12:00:00 -0700

    added str-ends-with.js

---
+++ 7kyu/strings/str-ends-with.js
@@ -0,0 +1,17 @@
+/* CHALLENGE DESCRIPTION
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+// function that takes in two params, both str
+// return true / false depending on if second string in fact is the end of the first string
+
+// MY SOLUTION
+    // check if str1 ends with str2
+    // if so, return true
+    // else return false
+const endsWith = (str1, str2) => str1.endsWith(str2);
