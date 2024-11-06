171b686dce0e942f79ea7b091704747d6de65b85
Kristin Dailey <kristin.dailey02@gmail.com>
2024-11-06 09:50:05 -0800

    added get-num-from-str.js

---
+++ 8kyu/strings/get-num-from-str.js
@@ -0,0 +1,30 @@
+/* CHALLENGE DESCRIPTION
+
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+Function:
+getNumberFromString(s)
+*/
+
+// MY SOLUTION
+function getNumberFromString(s) {
+    return Number(s.split("").filter(char => !isNaN(char) && char !== " ").join(""));
+}
+
+// OTHER SOLUTIONS
+function getNumberFromString(s) {
+    return Number(s.split('').filter(a => a <= '9' && a >= '0').join(''))
+}
+
+const getNumberFromString = (s) => parseInt(s.split("").filter(x => "0123456789".includes(x)).join(""));
+
+function getNumberFromString(s) {
+    let result = '';
+    const digits = '0123456789';
+    for (let i = 0; i < s.length; i++) {
+      if (digits.includes(s[i])) {
+        result += s[i];
+      }
+    }
+    return Number(result);
+}
