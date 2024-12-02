ffca469ab3d95c31c18e128b43ade78d0ddc4657
Kristin Dailey <kristin.dailey02@gmail.com>
2024-12-02 12:00:00 -0800

    added test cases

--- 8kyu/arrays/convert-boolean-to-string.js
+++ 8kyu/arrays/convert-boolean-to-string.js
@@ -1,5 +1,6 @@
-// CHALLENGE DESCRIPTION
-// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+/* CHALLENGE DESCRIPTION
+Complete the method that takes a boolean value and return a "Yes" string for true or a "No" string for false.
+*/
 
 // MY SOLUTION
 function boolToWord(bool) {
@@ -9,6 +10,9 @@ function boolToWord(bool) {
       return "No";
     }
 } 
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
 
 // OTHER SOLUTION
 const boolToWord = bool => bool ? "Yes" : "No";
