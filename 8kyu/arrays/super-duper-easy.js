7ff5a59191107a55a516c7953ddedd6c5c7c6838
Kristin Dailey <kristin.dailey02@gmail.com>
2024-12-05 12:00:00 -0800

    updated solution

--- 8kyu/arrays/super-duper-easy.js
+++ 8kyu/arrays/super-duper-easy.js
@@ -1,5 +1,6 @@
-// CHALLENGE DESCRIPTION
-// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+/* CHALLENGE DESCRIPTION
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
 
 // MY SOLUTION
 function problem(x) {
