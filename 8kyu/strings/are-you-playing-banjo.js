cd5a31cbdb05a160f6d5c0a0065722450dfb01c0
Kristin Dailey <kristin.dailey02@gmail.com>
2024-12-07 12:00:00 -0800

    Updated description

--- 8kyu/strings/are-you-playing-banjo.js
+++ 8kyu/strings/are-you-playing-banjo.js
@@ -1,8 +1,8 @@
 /* CHALLENGE DESCRIPTION
 
-Create a function which answers the question "Are you playing banjo?".
+Create a function which answers the question "Are you playing banjo?."
 
-If your name starts with the letter "R" or lower case "r", you are playing banjo!
+If your name starts with the letter "R" or lowercase "r," you are playing banjo!
 
 The function takes a name as its only argument and returns one of the following strings:
 name + " plays banjo" 
