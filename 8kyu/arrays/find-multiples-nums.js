20ad9a3ab381861b94185eb622b36ccd8732bf05
Kristin Dailey <kristin.dailey02@gmail.com>
2024-12-28 12:00:00 -0800

    Updated solution

--- 8kyu/arrays/find-multiples-nums.js
+++ 8kyu/arrays/find-multiples-nums.js
@@ -2,7 +2,7 @@
 
 In this simple exercise, you will build a program that takes a value, integer, and returns a list of its multiples up to another value limit. 
 
-If limit is a multiple of the integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. 
+If the limit is a multiple of the integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. 
 
 The limit will always be higher than the base.
 
@@ -11,14 +11,14 @@ For example, if the parameters passed are (2, 6), the function should return [2,
 
 // MY SOLUTION
 function findMultiples(integer, limit) {
-    const result = [];
+    let result = [];
     for (let i = integer; i <= limit; i += integer) {
       result.push(i);
     }
     return result;
 }
 
-// OTHER SOLUTION
+// OTHER SOLUTIONS
 const findMultiples = (integer, limit) => Array.from({ length: Math.floor(limit / integer) }, (_, i) => (i + 1) * integer);
 
 const findMultiples = (integer, limit) => [...Array(Math.floor(limit / integer)).keys()].map(i => (i + 1) * integer);
