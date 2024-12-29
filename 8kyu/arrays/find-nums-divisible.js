31770e3df9086e3c4edacbd935b297e91d13a964
Kristin Dailey <kristin.dailey02@gmail.com>
2024-12-29 12:00:00 -0800

    Updated description

--- 8kyu/arrays/find-nums-divisible.js
+++ 8kyu/arrays/find-nums-divisible.js
@@ -2,12 +2,12 @@
 
 Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. The first argument is an array of numbers and the second is the divisor.
 
-Example(Input1, Input2 --> Output)
+Example (Input1, Input2 --> Output):
 [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
 */
 
 // MY SOLUTION
 const divisibleBy = (numbers, divisor) => numbers.filter(num => num % divisor === 0);
 
-// OTHER SOLUTIONS
+// OTHER SOLUTION
 const divisible_By = (numbers, divisor) => numbers.filter(number => !(number % divisor));
