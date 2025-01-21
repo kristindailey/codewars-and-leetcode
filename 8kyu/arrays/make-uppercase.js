c3907da82ca4e33c96870fa3ec16c8a89a8e4d77
Kristin Dailey <kristin.dailey02@gmail.com>
2025-01-21 15:15:59 -0800

    Added tests

--- 8kyu/arrays/make-uppercase.js
+++ 8kyu/arrays/make-uppercase.js
@@ -4,21 +4,15 @@ Write a function which converts the input string to uppercase.
 
 */
 
-/* PREP
-Parameters
-    // string
-Returns 
-    // uppercase string
-Examples 
-    // "Hello"
-        // "HELLO"
-    //  "mYnAmEiS"
-        // "MYNAMEIS"
-Pseudocode 
-    // function that takes in a string
-    // attach method that turns string to uppercase
-    // return uppercase string
-*/
+// function that takes in a string
+// return uppercase string 
+// function that takes in a string
+// attach method that turns string to uppercase
+// return uppercase string
 
 // MY SOLUTION
 const returnUpperCaseStr = str => str.toUpperCase();
+
+console.log(returnUpperCaseStr("Hello")); // "HELLO"
+console.log(returnUpperCaseStr("mYnAmEiS")); // "MYNAMEIS"
+console.log(returnUpperCaseStr("aBCDe")); // "ABCDE"
