954764fd7a4f417d3c671b3991a6be1f91a68efe
Kristin Dailey <kristin.dailey02@gmail.com>
2025-01-23 07:05:28 -0800

    Added tests

--- 8kyu/arrays/merge-two-sorted-arrays.js
+++ 8kyu/arrays/merge-two-sorted-arrays.js
@@ -1,6 +1,6 @@
 /* CHALLENGE DESCRIPTION
 
-You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in ascending order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
 You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more integers. If both arr1 and arr2 are empty, then just return an empty array.
 
@@ -14,9 +14,14 @@ Examples: (input -> output)
 
 // MY SOLUTION
 function mergeArrays(arr1, arr2) {
-    const mergedArr = [...new Set (arr1.concat(arr2))];
+    const mergedArr = [...new Set(arr1.concat(arr2))];
     return mergedArr.sort((a, b) => a - b);
 }
+
+console.log(mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]), [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]);
+console.log(mergeArrays([], []), []);
 
 // OTHER SOLUTIONS
 const mergeArrays = (arr1, arr2) => Array.from(new Set(arr1.concat(arr2).sort((a, b) => a - b)));
