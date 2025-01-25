f2afb1c4fd5ea463db91f60b0b72025829b50db7
Kristin Dailey <kristin.dailey02@gmail.com>
2025-01-25 12:00:00 -0800

    Updated description

--- 8kyu/strings/grasshopper-str.js
+++ 8kyu/strings/grasshopper-str.js
@@ -1,12 +1,12 @@
 /* CHALLENGE DESCRIPTION
 
-Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+Write a function that gives a personalized greeting. This function takes two parameters: name and owner.
 
 Use conditionals to return the proper message:
-
-Case	                 Return
-name equals owner	    "Hello boss"
-otherwise	            "Hello guest"
+	     
+Case:                     Return:
+name equals owner	->    "Hello boss"
+otherwise	        ->    "Hello guest"
 */
 
 /* PREP
