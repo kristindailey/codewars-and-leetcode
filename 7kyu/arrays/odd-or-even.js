d4ee4da0b3abc293003c06ce5395b1ae0cf77d58
Kristin Dailey <kristin.dailey02@gmail.com>
2025-02-24 12:00:00 -0800

    Added tests

--- 7kyu/arrays/odd-or-even.js
+++ 7kyu/arrays/odd-or-even.js
@@ -26,7 +26,10 @@ function oddOrEven(arr) {
     }
 }
 
-// OTHER SOLUTIONS
-const oddOrEven = arr => arr.reduce((a, b) => a + b, 0) % 2 ? "odd" : "even";
+console.log(oddOrEven([0, 1, 4]), "odd");
+console.log(oddOrEven([0, -1, -5]), "even");
 
-const evenOrOdd = arr => arr.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd";
+// OTHER SOLUTIONS
+// const oddOrEven = arr => arr.reduce((a, b) => a + b, 0) % 2 ? "odd" : "even";
+
+// const evenOrOdd = arr => arr.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd";
