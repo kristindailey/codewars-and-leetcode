e9a2e1b7099cddf5485d553d0c6f9b114d3114c2
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-03 14:25:48 -0700

    Updated description

--- leetcode/14.js
+++ leetcode/14.js
@@ -2,14 +2,14 @@
 
 Write a function to find the longest common prefix string amongst an array of strings.
 
-If there is no common prefix, return an empty string "".
+If there is no common prefix, return an empty string: "".
 
 Example 1:
-Input: strs = ["flower","flow","flight"]
+Input: strs = ["flower", "flow", "flight"]
 Output: "fl"
 
 Example 2:
-Input: strs = ["dog","racecar","car"]
+Input: strs = ["dog", "racecar", "car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
  
