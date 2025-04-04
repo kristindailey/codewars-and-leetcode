fa5c6e0fded88cb27a7cf93021a18d7e0020c681
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-04 09:25:30 -0700

    Added solution and tests

--- leetcode/14.js
+++ leetcode/14.js
@@ -18,3 +18,27 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters if it is non-empty.
 */
+
+const returnLongestCommonPrefix = (arr) => {
+    if (arr.length === 1) return arr[0];
+
+    let commonPrefix = "";
+    const firstStr = arr[0];
+
+    for (let i = 0; i < firstStr.length; i++) {
+        const char = firstStr[i];
+        for (let j = 1; j < arr.length; j++) {
+            if (i >= arr[j].length || arr[j][i] !== char) {
+                return commonPrefix;
+            }
+        }
+        commonPrefix += char;
+    }
+
+    return commonPrefix;
+};
+
+console.log(returnLongestCommonPrefix(["flower", "flow", "flight"]), "fl");
+console.log(returnLongestCommonPrefix(["dog", "racecar", "car"]), "");
+console.log(returnLongestCommonPrefix(["", "hello", "hey"]), "");
+console.log(returnLongestCommonPrefix(["one"]), "one");
