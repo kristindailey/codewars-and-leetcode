d6d74bb012861a6041ca663e5dcd2045e1d6694c
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-09 10:48:51 -0700

    Added hash map solution

--- leetcode/242.js
+++ leetcode/242.js
@@ -1,6 +1,6 @@
 /* 242: VALID ANAGRAM
 
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Given two strings s and t, return true if t is an anagram of s and false otherwise.
 
 Example 1:
 Input: s = "anagram", t = "nagaram"
@@ -13,12 +13,39 @@ Output: false
 Constraints:
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
-
-Follow-up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 */
 
-// SORTING SOLUTION
+// HASH MAP SOLUTION
 const isAnagram = (s, t) => {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const countS = {};
+    const countT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        countS[s[i]] = (countS[s[i]] || 0) + 1;
+        countT[t[i]] = (countT[t[i]] || 0) + 1; 
+    }
+
+    for (const key in countS) {
+        if (countS[key] !== countT[key]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isAnagram("racecar", "carrace"), true);
+console.log(isAnagram("jar", "jam"), false);
+
+// Time complexity: O(n + m)
+    // Where n is s.length and m is t.length
+// Space complexity: O(1)
+
+// SORTING SOLUTION
+const is_Anagram = (s, t) => {
     if (s.length !== t.length) {
         return false;
     }
@@ -29,10 +56,11 @@ const isAnagram = (s, t) => {
     return sortedS === sortedT;
 };
 
-console.log(isAnagram("anagram", "nagaram"), true);
-console.log(isAnagram("rat", "car"), false);
-console.log(isAnagram("racecar", "carrace"), true);
-console.log(isAnagram("jar", "jam"), false);
+console.log(is_Anagram("anagram", "nagaram"), true);
+console.log(is_Anagram("rat", "car"), false);
+console.log(is_Anagram("racecar", "carrace"), true);
+console.log(is_Anagram("jar", "jam"), false);
 
 // Time complexity: O(n log n + m log m)
+    // Where n is s.length and m is t.length
 // Space complexity: O(1)
