d3323e176ad96d8cf88c0f2177b7889ed23e9bbf
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-10 10:59:31 -0700

    Added hash map solution

--- leetcode/1.js
+++ leetcode/1.js
@@ -1,6 +1,6 @@
 /* 1: TWO SUM
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers, nums, and an integer, target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -24,29 +24,18 @@ Constraints:
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 Only one valid answer exists.
-
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
-// NOTE: BELOW IS A BRUTE FORCE SOLUTION.
+// arr of nums - never empty, always ints, whole nums, positive or negative, at least 2 elements, will always hit target
+// target - integer, positive or negative
+// return indices of nums equaling target -> []
+  // there will be exactly one solution
 
-// arr of nums, target num
-// return indices of nums that add up to target
-
-// arr of nums - not empty, always nums, whole nums, positive, at least 2 nums, will always hit target, num + self will never = target (only two nums)
-// same for target
-// return indices of nums -> []
-
-// [1, 2, 3, 4, 5], 9 => [3, 4]
-// [5, 6, 9], 11 => [0, 1]
-// [22, 7, 100, 5], 12 => [1, 3]  
-
-function findIndices(arr, target) {
-    // grab a num
-    for (let i = 0; i < arr.length; i++) {
-      // compare outside num to internal num
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[i] + arr[j] === target && i !== j) {
+// BRUTE FORCE SOLUTION
+function findIndices(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = 0; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target && i !== j) {
           return [i, j];
         }
       }
@@ -56,3 +45,31 @@ function findIndices(arr, target) {
 console.log(findIndices([1, 2, 3, 4, 5], 9), [3, 4]);
 console.log(findIndices([5, 6, 9], 11), [0, 1]);
 console.log(findIndices([22, 7, 100, 5], 12), [1, 3]);
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+
+// HASH MAP SOLUTION
+const twoSum = (nums, target) => {
+  const prevMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const difference = target - nums[i];
+
+    if (prevMap.has(difference)) {
+      return [prevMap.get(difference), i];
+    }
+
+    prevMap.set(nums[i], i);
+  }
+
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
+console.log(twoSum([3, 2, 4], 6), [1, 2]);
+console.log(twoSum([3, 3], 6), [0, 1]);
+
+
+// Time complexity: O(n)
+// Space complexity: O(n)
