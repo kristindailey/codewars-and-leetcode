394d6d0de7d7ae34e0e0747ee6e1889926827932
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-10 10:01:02 -0700

    Updated description

--- leetcode/242.js
+++ leetcode/242.js
@@ -1,6 +1,6 @@
 /* 242: VALID ANAGRAM
 
-Given two strings s and t, return true if t is an anagram of s and false otherwise.
+Given two strings, s and t, return true if t is an anagram of s and false otherwise.
 
 Example 1:
 Input: s = "anagram", t = "nagaram"
