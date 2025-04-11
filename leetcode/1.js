0bf0a7e9a95df39e6c44f396e042b4cdda7b9adc
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-11 12:00:00 -0700

    Updated solution

--- leetcode/1.js
+++ leetcode/1.js
@@ -70,6 +70,5 @@ console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
 console.log(twoSum([3, 2, 4], 6), [1, 2]);
 console.log(twoSum([3, 3], 6), [0, 1]);
 
-
 // Time complexity: O(n)
 // Space complexity: O(n)
