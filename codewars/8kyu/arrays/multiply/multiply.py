b6177fcc7b20a741ece2fa4ebf8bef40f9fe2f3c
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-12 12:00:00 -0700

    Add multiply.py

---
+++ codewars/8kyu/arrays/multiply/multiply.py
@@ -0,0 +1,12 @@
+""" 
+CHALLENGE DESCRIPTION
+
+This code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+    a * b
+"""
+
+# MY SOLUTION
+def multiply(a, b):
+    return a * b
