edff0d216724e12786d73533501f27869a6ce57c
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-13 12:00:00 -0700

    Add even-or-odd.py

---
+++ codewars/8kyu/arrays/even-or-odd/even-or-odd.py
@@ -0,0 +1,15 @@
+""" 
+CHALLENGE DESCRIPTION
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+    pass
+"""
+
+# MY SOLUTION
+def even_or_odd(number):
+    if number % 2:
+        return "Odd"
+    
+    return "Even"
