6e804f9cf11d2471403f431158a239cc3e99ffbf
Kristin Dailey <kristin.dailey02@gmail.com>
2025-04-20 12:00:00 -0700

    Add reversed-strings.py

---
+++ codewars/8kyu/strings/reversed-strings/reversed-strings.py
@@ -0,0 +1,13 @@
+""" 
+CHALLENGE DESCRIPTION
+
+Complete the solution so that it reverses the string passed into it.
+
+"world"  =>  "dlrow"
+"word"   =>  "drow"
+"""
+
+# MY SOLUTION
+def reverse_string(string):
+    reversed_string = string[::-1]
+    return reversed_string
