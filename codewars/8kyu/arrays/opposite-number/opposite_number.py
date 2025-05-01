ccbbefe054dfff0b12876fe7dea1d6b1381e2ede
Kristin Dailey <kristin.dailey02@gmail.com>
2025-05-01 12:00:00 -0700

    Add opposite_number.py

---
+++ codewars/8kyu/arrays/opposite-number/opposite_number.py
@@ -0,0 +1,14 @@
+""" 
+CHALLENGE DESCRIPTION
+
+Given a number, return its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+-34: 34
+"""
+
+# MY SOLUTION
+def opposite(number):
+    return -number
