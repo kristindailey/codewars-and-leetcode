efb18cd39c6892e190b951ef12175ef2f21b1389
Kristin Dailey <kristin.dailey02@gmail.com>
2025-05-12 12:00:00 -0700

    Add string_repeat.py

---
+++ codewars/8kyu/strings/string-repeat/string_repeat.py
@@ -0,0 +1,13 @@
+""" 
+CHALLENGE DESCRIPTION
+
+Write a function that accepts an integer and a string as parameters and returns a string repeated exactly n times.
+
+Examples: (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+"""
+
+# MY SOLUTION
+def repeat_str(repeat, string):
+    return string * repeat
