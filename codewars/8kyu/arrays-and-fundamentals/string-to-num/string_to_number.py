5fe4f3c4bc7027598ec22d78acc3c25458607115
Kristin Dailey <kristin.dailey02@gmail.com>
2025-05-31 12:00:00 -0700

    Add string_to_number.py

---
+++ codewars/8kyu/arrays-and-fundamentals/string-to-num/string_to_number.py
@@ -0,0 +1,11 @@
+""" 
+CHALLENGE DESCRIPTION
+
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: All inputs will be strings, and every string is a perfectly valid representation of an integral number.
+"""
+
+# MY SOLUTION
+def string_to_number(str):
+    return int(str)
