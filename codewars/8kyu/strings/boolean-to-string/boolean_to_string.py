ca8b5aab9ea0e0f5e62b45343076db144a260c77
Kristin Dailey <kristin.dailey02@gmail.com>
2025-06-17 12:00:00 -0700

    Add boolean_to_string.py

---
+++ codewars/8kyu/strings/boolean-to-string/boolean_to_string.py
@@ -0,0 +1,11 @@
+""" 
+CHALLENGE DESCRIPTION
+
+Implement a function which converts the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+"""
+
+# MY SOLUTION
+def boolean_to_string(bool):
+    return str(bool)
